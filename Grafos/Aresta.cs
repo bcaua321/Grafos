5d54e509f0e7d402fcaa72071470d1e59d99a94d
Breno <bcaua205@gmail.com>
2022-11-02 21:23:10 -0300

    Adicionando Aresta

---
+++ Grafos/Aresta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafos
+{
+    public class Aresta
+    {
+        public int VerticeUm { get; set; }
+        public int VerticeDois { get; set; }
+        public int Peso { get; set; }
+
+        public Aresta(int verticeUm, int verticeDois)
+        {
+            VerticeUm = verticeUm;
+            VerticeDois = verticeDois;
+        }
+
+        public Aresta(int verticeUm, int verticeDois, int peso)
+        {
+            VerticeUm = verticeUm;
+            VerticeDois = verticeDois;
+            Peso = peso;
+        }
+    }
+}
